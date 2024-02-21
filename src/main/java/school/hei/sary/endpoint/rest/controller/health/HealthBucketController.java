816436881dd678a379f11588be212b9b3c8bab63
Tombosoa <hei.nantenaina.4@gmail.com>
2024-02-21 17:53:37 +0300

    feat: change vpc

--- src/main/java/school/hei/sary/endpoint/rest/controller/health/HealthBucketController.java
+++ src/main/java/school/hei/sary/endpoint/rest/controller/health/HealthBucketController.java
@@ -5,20 +5,23 @@ import static java.nio.file.Files.createTempDirectory;
 import static java.util.UUID.randomUUID;
 import static school.hei.sary.file.FileHashAlgorithm.NONE;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
 import java.time.Duration;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import school.hei.sary.PojaGenerated;
 import school.hei.sary.file.BucketComponent;
 import school.hei.sary.file.FileHash;
+
+import javax.imageio.ImageIO;
 
 @PojaGenerated
 @RestController
@@ -80,5 +83,62 @@ public class HealthBucketController {
 
   private URL can_presign(String fileBucketKey) {
     return bucketComponent.presign(fileBucketKey, Duration.ofMinutes(2));
+  }
+
+  @PostMapping(value = "/uploadPictures", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+  public ResponseEntity<String> uploadToBucket(
+          @RequestParam MultipartFile file,
+          @RequestParam String key
+  ) throws IOException {
+    File localFile = convertMultipartFileToFile(file);
+    bucketComponent.upload(localFile, HEALTH_KEY + key);
+    localFile.delete();
+    return ResponseEntity.ok("Pictures uploaded");
+  }
+
+  @GetMapping("pictures/{key}")
+  public ResponseEntity<byte[]> getPicturesFromBucket(@PathVariable String key){
+    byte[] pictureByte = bucketComponent.download(HEALTH_KEY+key).toString().getBytes();
+
+    return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(pictureByte);
+  }
+
+  public ResponseEntity<byte[]> getPicturesBlackAndWhite(
+          @PathVariable String key
+  ){
+    byte[] originalPictureByte = bucketComponent.download(HEALTH_KEY+key).toString().getBytes();
+
+    byte[] blackAndWhitePictureByte = convertToBW(originalPictureByte);
+
+    return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(blackAndWhitePictureByte);
+  }
+
+  private File convertMultipartFileToFile(MultipartFile file) throws IOException {
+    File localFile = File.createTempFile("tmp", file.getOriginalFilename());
+    file.transferTo(localFile);
+    return localFile;
+  }
+
+  private byte[] convertToBW(byte[] originalPictureByte){
+    try {
+      ByteArrayInputStream bais = new ByteArrayInputStream(originalPictureByte);
+      BufferedImage originalImage = ImageIO.read(bais);
+
+      BufferedImage BWImage = new BufferedImage(
+              originalImage.getWidth(),
+              originalImage.getHeight(),
+              BufferedImage.TYPE_BYTE_GRAY
+      );
+      Graphics g = BWImage.getGraphics();
+      g.drawImage(originalImage, 0, 0, null);
+
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      ImageIO.write(BWImage, "jpg", baos);
+
+      return baos.toByteArray();
+    }catch (IOException e) {
+      e.printStackTrace();
+      return originalPictureByte;
+    }
   }
 }
